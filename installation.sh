8122d657da7b6e1a86f109b40eed73c5d0e6d67b
root <root@DESKTOP-GDNLET7.localdomain>
2023-05-20 09:11:21 +0900

    update: インストール簡易化

---
+++ installation.sh
@@ -0,0 +1,7 @@
+
+PROJECT_PATH=$(cd $(dirname $0);pwd)
+PROJECT_DIR=`echo ${PROJECT_PATH##*/}`
+
+cp $PROJECT_PATH/.env.develop $PROJECT_PATH/.env
+# docker compose exec app /bin/sh -c "cd ${PROJECT_DIR} && composer update"
+chmod 777 -R ${PROJECT_PATH}
