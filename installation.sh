09f0aa897169c82ec19b2a4946bfe527ce710554
root <root@DESKTOP-GDNLET7.localdomain>
2023-05-21 12:35:16 +0900

    インストール用シェルスクリプトのコメントアウト解除

--- installation.sh
+++ installation.sh
@@ -3,5 +3,5 @@ PROJECT_PATH=$(cd $(dirname $0);pwd)
 PROJECT_DIR=`echo ${PROJECT_PATH##*/}`
 
 cp $PROJECT_PATH/.env.develop $PROJECT_PATH/.env
-# docker compose exec app /bin/sh -c "cd ${PROJECT_DIR} && composer update"
+docker compose exec app /bin/sh -c "cd ${PROJECT_DIR} && composer update"
 chmod 777 -R ${PROJECT_PATH}
